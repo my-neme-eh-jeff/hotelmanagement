27bb0e4f22a4b91b890a756def988a1e21b55be9
jeff <aman2003nambisan@gmail.com>
2024-03-09 12:37:33 +0530

    initial comit

---
+++ siteConfig.tsx
@@ -0,0 +1,9 @@
+import { FileCheck2 } from "lucide-react";
+
+export const siteConfig = {
+  name: "Docstruct",
+  description: "PlanHarbor",
+  url: "https://Docstruct.vercel.app/",
+  logo: <FileCheck2 />,
+  baseUrl: "http://localhost:8081",
+};
