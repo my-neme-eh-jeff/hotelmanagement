a63264e44de897daa58c7f63c4c99b0d790c6710
jeff <aman2003nambisan@gmail.com>
2024-03-10 10:58:43 +0530

    feat: shceduling checks started

--- src/app/admin/scheduling-checks/page.tsx
+++ src/app/admin/scheduling-checks/page.tsx
@@ -2,18 +2,39 @@
 import { useEffect, useState } from "react";
 import { Button, Image, Select, Modal } from "antd";
 import { Tooltip } from "@nextui-org/tooltip";
+import axios from "axios";
+import { ScaleLoader } from "react-spinners";
 
 export default function SchedulingChecks() {
   const [floor, setFloor] = useState(1);
-  const [rooms, setRooms] = useState(8);
+  const [rooms, setRooms] = useState([]);
   const [open, setOpen] = useState(false);
-  const heightInREM = 24;
+  const [presentRoom, setPresentRoom] = useState();
+  const [dataFromApi, setDataFromApi] = useState();
+  const [floorOptions, setFloorOptions] = useState([]); // Initialize state to hold floor options
   const [loading, setLoading] = useState(true);
-  useEffect(()=>{
-    async function getData(){
-      
+  const [assignedStaff, setAssignedStaff] = useState();
+
+  useEffect(() => {
+    async function getData() {
+      try {
+        setLoading(true);
+        const { data } = await axios.get(
+          "https://loc6.pythonanywhere.com/inventory/room/"
+        );
+        const floors = Array.from(new Set(data.map((item) => item.floor))).map(
+          (floor) => ({ value: floor.toString(), label: `Floor ${floor}` })
+        );
+        setFloorOptions(floors);
+        setDataFromApi(data);
+      } catch (err) {
+        console.log(err);
+      } finally {
+        setLoading(false);
+      }
     }
-  },[])
+    getData();
+  }, []);
   const showModal = () => {
     setOpen(true);
   };
@@ -23,25 +44,59 @@ export default function SchedulingChecks() {
   const handleCancel = () => {
     setOpen(false);
   };
-  const DoorComponent = ({ leftOrNot }: { leftOrNot: boolean }) => {
+  const getAssignedStaff = async (roomId: number) => {
+    const { data } = await axios.get(
+      `https://loc6.pythonanywhere.com/inventory/staff/${roomId}`
+    );
+    setAssignedStaff({ name: data.name, staffType: data.staff_type });
+    console.log(data);
+  };
+  const setCurrentRoom = async (id: number) => {
+    await getAssignedStaff(id);
+    setPresentRoom((prev) => rooms.filter((room) => room.id === id)[0]);
+    showModal();
+  };
+  const DoorComponent = ({
+    leftOrNot,
+    room,
+  }: {
+    leftOrNot: boolean;
+    room: any;
+  }) => {
     return (
       <Tooltip
         placement={leftOrNot ? "left" : "right"}
         content={
           <div className="justify-center relative p-4 pt-2 overflow-hidden rounded-xl border border-neutral-300 bg-neutral-200 dark:border-neutral-700 dark:bg-cyan-300 bg-[linear-gradient(45deg,transparent_25%,rgba(68,68,68,.7)_50%,transparent_75%,transparent_100%)] bg-[length:250%_250%,100%_100%] bg-[position:-100%_0,0_0] bg-no-repeat shadow-2xl dark:shadow-zinc-900 hover:bg-[position:200%_0,0_0] hover:duration-[1500ms]">
             <div className="flex justify-center text-small font-semibold">
-              Room 101
+              Room {room.id}
             </div>
-            <div className="text-tiny">Last occupant: Jannat Singh</div>
-            <div className="text-tiny">Last checkin date: 09/03/2024</div>
-            <div className="text-tiny">Phone Number: 981383125</div>
-            <Button onClick={showModal}>View More</Button>
+            <div className="text-tiny">
+              {room.room_details.ocuupied ? "Current " : "Previous "}occupant:{" "}
+              {room.room_details.occupant}
+            </div>
+            <div className="text-tiny">
+              Last checkin date: {room.room_details["checkin date"]}
+            </div>
+            <div className="text-tiny">
+              Phone Number: {room.room_details.phonenumber}
+            </div>
+            <Button
+              onClick={() => {
+                setCurrentRoom(room.id);
+                console.log(presentRoom);
+              }}
+            >
+              View More
+            </Button>
           </div>
         }
       >
         <div className="flex w-[35px] h-[20px]">
           <div
-            className={`flex-1 bg-[#964B00] w-[35px] h-[20px] relative border border-dashed`}
+            className={`flex-1 bg-[#964B00] w-[35px] h-[20px] relative ${
+              room.room_details.ocuupied ? "border-green-400 border-dashed border-2" : "border-"
+            }`}
             style={{
               borderRadius: `${
                 leftOrNot ? "4px 0px 0px 4px" : "0px 4px 4px 0px"
@@ -60,75 +115,77 @@ export default function SchedulingChecks() {
       </Tooltip>
     );
   };
-  const RoomDoors = ({ leftOrNot }: { leftOrNot: boolean }) => {
+  const handlingFloorChange = (value) => {
+    setFloor(parseInt(value));
+    const roomsOnFloor = dataFromApi.filter(
+      (room) => room.floor === parseInt(value)
+    );
+    setRooms(roomsOnFloor);
+    console.log(roomsOnFloor);
+  };
+
+  const RoomDoors = ({ leftOrNot }) => {
+    const minGap = 6;
+    const maxGap = 24;
+    const dynamicGap = Math.max(minGap, maxGap - rooms.length);
     return (
-      <div className={`py-6 flex flex-col gap-y-6`}>
-        {Array.from({ length: rooms }, (_, index) => (
-          <div key={index}>
-            <DoorComponent leftOrNot={leftOrNot} />
-          </div>
-        ))}
+      <div
+        className={`py-10 flex flex-col align-middle`}
+        style={{ gap: `${dynamicGap}px` }} // Apply dynamic gap here
+      >
+        {rooms.length > 1 &&
+          rooms.map((room, index) => (
+            <div key={index}>
+              <DoorComponent leftOrNot={leftOrNot} room={room} />
+            </div>
+          ))}
       </div>
     );
   };
 
   return (
     <>
-      <div className="flex flex-col justify-center mt-12 mb-6 gap-10">
-        <Select
-          className="w-80 !mx-auto justify-center"
-          showSearch
-          onChange={(value) => setFloor(value)}
-          placeholder="Select Floor"
-          optionFilterProp="children"
-          filterOption={(input, option) =>
-            (option?.label ?? "").includes(input)
-          }
-          filterSort={(optionA, optionB) =>
-            (optionA?.label ?? "")
-              .toLowerCase()
-              .localeCompare((optionB?.label ?? "").toLowerCase())
-          }
-          options={[
-            {
-              value: "1",
-              label: "Floor 1",
-            },
-            {
-              value: "2",
-              label: "Floor 2",
-            },
-            {
-              value: "3",
-              label: "Floor 3",
-            },
-            {
-              value: "4",
-              label: "Floor 4",
-            },
-            {
-              value: "5",
-              label: "Floor 5",
-            },
-            {
-              value: "6",
-              label: "Floor 6",
-            },
-          ]}
-        />
-        <div className="flex flex-row justify-center h-96 justify-items-center">
-          <div className="">
-            <RoomDoors leftOrNot={true} />
-          </div>
-          <div className="w-36 border-2 border-solid border-indigo-600 rounded-xl text-center"></div>
-          <div className="">
-            <RoomDoors leftOrNot={false} />
+      {loading ? (
+        <div className="flex justify-center items-center">
+          <ScaleLoader color="#2563eb" />
+        </div>
+      ) : (
+        <div className="flex flex-col justify-center mt-12 mb-6 gap-10">
+          
+          {!loading && (
+            <Select
+              className="w-80 !mx-auto justify-center"
+              showSearch
+              onChange={(value) => handlingFloorChange(value)}
+              placeholder="Select Floor"
+              optionFilterProp="children"
+              filterOption={(input, option) =>
+                (option?.label ?? "")
+                  .toLowerCase()
+                  .includes(input.toLowerCase())
+              }
+              filterSort={(optionA, optionB) =>
+                (optionA?.label ?? "")
+                  .toLowerCase()
+                  .localeCompare((optionB?.label ?? "").toLowerCase())
+              }
+              options={floorOptions}
+            />
+          )}
+          <div className="flex flex-row justify-center h-96 justify-items-center">
+            <div>
+              <RoomDoors leftOrNot={true} />
+            </div>
+            <div className="w-36 border-2 border-solid border-indigo-600 rounded-xl text-center"></div>
+            <div>
+              <RoomDoors leftOrNot={false} />
+            </div>
           </div>
         </div>
-      </div>
+      )}
       <Modal
         open={open}
-        title="Room 101"
+        title={`Room Details`}
         onOk={handleOk}
         onCancel={handleCancel}
         footer={(_, { OkBtn, CancelBtn }) => (
@@ -138,15 +195,24 @@ export default function SchedulingChecks() {
           </>
         )}
       >
-        <div className="mb-4 rounded-md flex justify-center place-items-center place-content-center">
-          <Image
-            width={300}
-            className="rounded-md"
-            alt="A bunch of pink and white flowers in a vase"
-            src="https://images.unsplash.com/photo-1709891798937-fd431bd7e10b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          />
-        </div>
-        <p>Captured at: 08/03/2024</p>
+        <Image.PreviewGroup>
+          {presentRoom &&
+            Object.values(presentRoom.room_details["latest image"]).map(
+              (imgUrl, index) => (
+                <Image
+                  key={index}
+                  src={imgUrl}
+                  alt={`Room Image ${index + 1}`}
+                  style={{ width: "100%", marginBottom: 8 }}
+                />
+              )
+            )}
+        </Image.PreviewGroup>
+        {presentRoom && <p>Captured at: 10/03/2024</p>}
+        {"Assigned staff: " +
+          assignedStaff?.name +
+          ", " +
+          assignedStaff?.staffType}
       </Modal>
     </>
   );
